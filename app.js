8a5bf92c4f3681fef52f56e4476af5b9fb475731
Shahriyor006 <abdujalilovshaxriyor3@gmail.com>
2023-07-27 14:45:59 +0500

    navbar media bilan

---
+++ app.js
@@ -0,0 +1,85 @@
+console.log("salom")
+
+// Data types
+// 1. Primitive
+//      1. number - 5, -7, 0
+//      2. string - hello, lorem
+//      3. boolean - true. false
+//      4. undefined - undefined
+//      5. null
+//      6. bigint
+//      7.symbol
+// 2. Reference
+//      1. object
+
+let num = 15
+console.log( typeof(num) );
+
+let str = "good"
+console.log( typeof(str) );
+
+let bool = false
+console.log( typeof bool );
+
+let empty
+console.log( empty );
+console.log( typeof empty );
+
+let user = null
+console.log( user );
+console.log( typeof user );
+
+
+
+
+// let ikki = 20
+// ikki = 10
+// console.log(ikki);
+// ikki = 15
+
+
+// camelCase
+// let loremIpsumDolor = 2;
+
+//
+// let LoremIpsumDolor = 3;
+
+// let a = 100, b = 200, c = 10
+
+// console.log(a + b * c);
+
+
+
+// alert("salom")
+// alert("xayr")
+
+
+
+// let ism = prompt("your name")
+// console.log( ism );
+
+let poo="hello"
+console.log( poo + " world" );
+console.log( 8 + 2 );
+
+console.log( "8" + 2 ); //82
+console.log( "8" - 2 ); //6
+console.log( "8" * 2 ); //16
+console.log( "8" / 2 ); //4
+
+console.log( "hello" + 2 ); // hello2
+console.log( "hello" - 2 ); // NaN - not a number
+console.log( "hello" * 2 ); // NaN - not a number
+console.log( "hello" / 2 ); // NaN - not a number
+
+
+
+
+
+
+
+
+
+
+
+
